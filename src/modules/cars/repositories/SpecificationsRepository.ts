424601a52c377a5d84fcfc286e568c063814e59b
Bruno Nielsen <programandowithjava@gmail.com>
2021-03-20 01:37:32 -0300

    :constructor: refat. routes..

--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -14,7 +14,7 @@ class SpecificationsRepository implements ISpecificationsRepository {
   create({ name, description }: ICreateSpecificationDTO): void {
     const specification = new Specification();
 
-    Object.assign(Specification, {
+    Object.assign(specification, {
       name,
       description,
       created_at: new Date(),
