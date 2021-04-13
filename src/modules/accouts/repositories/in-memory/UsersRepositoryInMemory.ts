6e1601f611e103de77abff0ea069db3b61db9078
Bruno Nielsen <programandowithjava@gmail.com>
2021-04-13 13:39:51 -0300

    :construction: Criando migration Cars

--- src/modules/accouts/repositories/in-memory/UsersRepositoryInMemory.ts
+++ src/modules/accouts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -1,5 +1,6 @@
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { User } from "../../infra/typeorm/entities/User";
+import { ICreateUserDTO } from "@modules/accouts/dtos/ICreateUserDTO";
+import { User } from "@modules/accouts/infra/typeorm/entities/User";
+
 import { IUsersRepository } from "../IUsersRepository";
 
 class UsersRepositoryInMemory implements IUsersRepository {
