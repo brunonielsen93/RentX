03007d1b2ba3223ad62f608e4424450d3bcfc32b
Bruno Nielsen <programandowithjava@gmail.com>
2021-04-18 09:59:48 -0300

    :construction:Criando Migration Rentals

--- src/shared/infra/typeorm/migrations/1617250076140-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1617250076140-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1617250076140 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
