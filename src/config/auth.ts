7a979f2d1ea74a577f41dcaba016c9cd262db952
Bruno Nielsen <programandowithjava@gmail.com>
2021-05-15 22:49:00 -0300

    Refactoring Authentications Users

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "a7e071b3de48cec1dd24de6cbe6c7bf1",
+  expires_in_token: "15min",
+  secret_refresh_token: "d3a419ad52ac6061e3d56568467696b5",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
